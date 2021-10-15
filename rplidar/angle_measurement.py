3f688bf9cfb85f34a808a405f96823cace9184f4
speae <speae147258@daum.net>
2021-10-15 16:15:18 +0900

    data delete

--- rplidar/angle_measurement.py
+++ rplidar/angle_measurement.py
@@ -12,10 +12,6 @@ dy = int(y2) - int(y1)
 radian = math.atan2(dy, dx)
 degree = math.degrees(radian)
 
-sin_radian = math.sin(radian)
-sin_degree = math.degrees(sin_radian)
-
 myRPlidar_degree = 90 - degree
 
 print(myRPlidar_degree)
-print(sin_degree)
